7c4bd01422c93fa981f0ac88f12226cf4969da8a
sdjeon <sdjeon_3top@gsretail.com>
2023-05-25 10:58:52 +0900

    sec7.4 validation

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,27 @@
 import { Repository } from 'typeorm';
 import { CustomRepository } from 'src/configs/typeorm-ex.decorator';
-import { CreateUserDto } from './dto/create-user.dto';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import { BadRequestException, ConflictException } from '@nestjs/common';
 
 @CustomRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({
+      username,
+      password,
+    });
+
+    try {
+      return await this.save(user);
+    } catch (e) {
+      if (e.code === '23505') {
+        throw new ConflictException(`${username} aleady exists.`);
+      } else {
+        throw new BadRequestException();
+      }
+    }
+  }
+}
