caa5a5e1a93037ee06385c848c9d95a1b0bd0f03
sdjeon <sdjeon_3top@gsretail.com>
2023-05-25 14:07:22 +0900

    sec7.6 encryption

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -3,15 +3,19 @@ import { CustomRepository } from 'src/configs/typeorm-ex.decorator';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 import { BadRequestException, ConflictException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<User> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.create({
       username,
-      password,
+      password: hashedPassword,
     });
 
     try {
