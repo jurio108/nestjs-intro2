678c2a53ceb1cd742201e11eec79c428fd27b59c
sdjeon <sdjeon_3top@gsretail.com>
2023-05-26 14:02:08 +0900

    sec10 configuration

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,13 +1,16 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Board } from 'src/boards/board.entity';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'tiger',
-  database: 'board-app',
+  type: dbConfig.type,
+  host: process.env.DB_HOSTNAME || dbConfig.host,
+  port: process.env.DB_PORT || dbConfig.port,
+  username: process.env.DB_USERNAME || dbConfig.username,
+  password: process.env.DB_PASSWORD || dbConfig.password,
+  database: process.env.DB_DATABASE || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}', Board],
-  synchronize: true,
+  synchronize: process.env.SYNCHRONIZE || dbConfig.synchronize,
 };
